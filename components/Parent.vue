c93ab28b11e7eced41af838232d3cca52430e38b
TetoneHaruki <haruki_tetone@schoo.jp>
2025-01-14 10:07:10 +0900

    Parentコンポーネントのh1要素の余分なスペースを削除し、Childコンポーネントを明示的にインポート

--- components/Parent.vue
+++ components/Parent.vue
@@ -1,6 +1,9 @@
+<script setup lang="ts">
+import Child from './Child.vue';
+</script>
 <template>
     <div>
-        <h1 class="parent">Parent </h1>
+        <h1 class="parent">Parent</h1>
         <Child />
     </div>
 </template>
